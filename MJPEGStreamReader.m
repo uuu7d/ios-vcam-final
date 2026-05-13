975c6fc147d8d8d7e20e332e5cc749d1b2354c7c
MurkAskA1 <AlisonPer1@proton.me>
2026-05-13 10:54:36 -0400

    Version 247.0: Diagnostic Master Upgrade. Part 2: Robust Implementation.

--- MJPEGStreamReader.m
+++ MJPEGStreamReader.m
@@ -1,4 +1,4 @@
-// MJPEGStreamReader.m - VirtualCamPro V246.0
+// MJPEGStreamReader.m - VirtualCamPro V247.0
 #import "MJPEGStreamReader.h"
 #import <objc/runtime.h>
 
@@ -33,7 +33,6 @@ static void VCamLog(NSString *format, ...) {
         _streamURL = url;
         _receiveBuffer = [NSMutableData data];
         _parseQueue = dispatch_queue_create("com.vcam.mjpeg.parse", DISPATCH_QUEUE_SERIAL);
-        VCamLog(@"Reader init: %@", url);
     }
     return self;
 }
@@ -41,10 +40,20 @@ static void VCamLog(NSString *format, ...) {
 - (void)startStreaming {
     [self stopStreaming];
     self.isConnecting = YES;
-    VCamLog(@"Starting session...");
+    VCamLog(@"Starting session for %@", self.streamURL);
+    
     NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
     config.requestCachePolicy = NSURLRequestReloadIgnoringLocalCacheData;
     config.timeoutIntervalForResource = 0;
+    
+    // ATS Bypass on session level
+    @try {
+        SEL sel = NSSelectorFromString(@"_setAllowsArbitraryLoads:");
+        if ([config respondsToSelector:sel]) {
+            ((void (*)(id, SEL, BOOL))objc_msgSend)(config, sel, YES);
+        }
+    } @catch (NSException *e) {}
+
     self.session = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:nil];
     self.dataTask = [self.session dataTaskWithURL:self.streamURL];
     [self.dataTask resume];
@@ -65,14 +74,18 @@ static void VCamLog(NSString *format, ...) {
         NSUInteger len = self.receiveBuffer.length;
         
         NSInteger soi = -1;
-        for (NSUInteger i = 0; i <= (len > 2 ? len - 2 : 0); i++) {
-            if (bytes[i] == 0xFF && bytes[i+1] == 0xD8) { soi = i; break; }
+        if (len > 2) {
+            for (NSUInteger i = 0; i <= len - 2; i++) {
+                if (bytes[i] == 0xFF && bytes[i+1] == 0xD8) { soi = i; break; }
+            }
         }
         
         if (soi != -1) {
             NSInteger eoi = -1;
-            for (NSUInteger i = soi + 2; i <= (len > 2 ? len - 2 : 0); i++) {
-                if (bytes[i] == 0xFF && bytes[i+1] == 0xD9) { eoi = i; break; }
+            if (len > soi + 2) {
+                for (NSUInteger i = soi + 2; i <= len - 2; i++) {
+                    if (bytes[i] == 0xFF && bytes[i+1] == 0xD9) { eoi = i; break; }
+                }
             }
             
             if (eoi != -1) {
@@ -81,18 +94,24 @@ static void VCamLog(NSString *format, ...) {
                 if (img) {
                     self.frameCount++;
                     self.lastFrameTime = CFAbsoluteTimeGetCurrent();
-                    if (self.frameCallback) self.frameCallback(img);
+                    if (self.frameCallback) {
+                        dispatch_async(dispatch_get_main_queue(), ^{ self.frameCallback(img); });
+                    }
                 }
                 [self.receiveBuffer replaceBytesInRange:NSMakeRange(0, eoi + 2) withBytes:NULL length:0];
             }
         }
-        if (self.receiveBuffer.length > 1024 * 1024 * 10) [self.receiveBuffer setLength:0];
+        if (self.receiveBuffer.length > 1024 * 1024 * 8) [self.receiveBuffer setLength:0];
     });
 }
 
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
     if (error) VCamLog(@"Session error: %@", error.localizedDescription);
-    if (self.isConnecting) [self performSelector:@selector(startStreaming) withObject:nil afterDelay:3.0];
+    if (self.isConnecting) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self startStreaming];
+        });
+    }
 }
 
 @end
